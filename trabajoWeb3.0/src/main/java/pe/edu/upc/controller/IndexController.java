a5eae42f570cf2106c323cd7818b15f21b9de319
Arthas5 <71291062+Arthas5@users.noreply.github.com>
2020-10-25 11:58:40 -0500

    Mi avance

---
+++ trabajoWeb3.0/src/main/java/pe/edu/upc/controller/IndexController.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.controller;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+public class IndexController {
+@GetMapping("/")
+public String home() {
+    return "index";
+}
+}
