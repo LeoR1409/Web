53bd0d33199263ddfce9143b37bf94672fc5deb5
LRamirez99 <67750855+LRamirez99@users.noreply.github.com>
2020-11-10 14:55:03 -0500

    usuarios creados

--- trabajoWeb3.0/src/main/resources/data.sql
+++ trabajoWeb3.0/src/main/resources/data.sql
@@ -2,3 +2,23 @@ INSERT INTO typeuser (typeuser_name)    VALUES ('usuario');
 INSERT INTO users(
 	email, namecity, namecountry, numphone, password, username, id_type_user)
 	VALUES ('holi@gmail.com', 'lima', 'peru', '345', '$2a$10$K0JKuL56.eIQ4iRbyaZqI.XJbn7/Fp9EQ0UZUXHUPupECpD5WgrTW', 'web', 1);
+	
+INSERT INTO users(
+	email, namecity, namecountry, numphone, password, username, id_type_user)
+	VALUES ('carloscolina@gmail.com', 'Lima', 'Perú', '955149997', '$2a$10$QIaTn1mHDbDu2vFHtrUg4e73WX1jI/fE1toqD87VdAAU6JU.NzkI.', 'carlos', 1);
+	
+INSERT INTO users(
+	email, namecity, namecountry, numphone, password, username, id_type_user)
+	VALUES ('jeff@gmail.com', 'Lima', 'Perú', '955149997', '$2a$10$ujhF5wC1WubEyoJS4KdS2OtJi2mECV6RZOcl.0HWRaTW6O5O7RgIG', 'jeff', 1);
+	
+INSERT INTO users(
+	email, namecity, namecountry, numphone, password, username, id_type_user)
+	VALUES ('jorgito@gmail.com', 'Lima', 'Perú', '955149997', '$2a$10$knI0BJmatfPSCTgPcrnroOhUhNnfQnnwi6XcclLBJERt4DbrC/Bx2', 'jorgito', 1);
+	
+INSERT INTO users(
+	email, namecity, namecountry, numphone, password, username, id_type_user)
+	VALUES ('diego@gmail.com', 'Lima', 'Perú', '955149997', '$2a$10$wMUJGPi3.0HvKihUEJgqpuD4rdfh7AMADFmw81Ej5w2vyl9V0aK4m', 'diego', 1);
+	
+INSERT INTO users(
+	email, namecity, namecountry, numphone, password, username, id_type_user)
+	VALUES ('leo@gmail.com', 'Lima', 'Perú', '955149997', '$2a$10$D//FQCh5TuKF58savWqAbucEUXZo9tFO74Zy2sYtBSRjn34wBkp3O', 'leo', 1);
